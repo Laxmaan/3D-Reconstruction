562285c18951db789a43961598598bbefda13992
Unknown <laxmaan15018@cse.ssn.edu.in>
2020-05-05 17:14:31 -0400

    Fixed Z buffer

--- get_Z_buffer.m
+++ get_Z_buffer.m
@@ -5,7 +5,7 @@ function [zbuffer] = get_Z_buffer(points_3d,points_2d,K,R_t)
 R = R_t(:,1:3);
 T = R_t(:,4);
 
-pt = points_2d(:,1); %use one point to get u
+pt = mean(points_2d,2); %use one point to get u
 u = R'*pinv(K)* (pt/norm(pt));
 
 u_hat = u / norm(u);
